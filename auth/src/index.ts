b72e5787f0056a92be09b7dcdc6e42571b136423
stephen grider <ste.grider@gmail.com>
2020-05-13 11:11:46 -0600

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+  console.log('Starting up...');
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
