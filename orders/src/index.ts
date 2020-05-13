7736bd4672c91ab0371179f9ab405d4cff96ffb7
stephen grider <ste.grider@gmail.com>
2020-05-13 11:48:53 -0600

    add startup message to orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,8 @@ import { ExpirationCompleteListener } from './events/listeners/expiration-comple
 import { PaymentCreatedListener } from './events/listeners/payment-created-listener';
 
 const start = async () => {
+  console.log('Starting...');
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
