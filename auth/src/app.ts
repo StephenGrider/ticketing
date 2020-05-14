e4637036649bd93e3188cc358e539c9ef8279071
stephen grider <ste.grider@gmail.com>
2020-05-14 13:00:12 -0600

    remove https req

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -15,7 +15,8 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    // secure: process.env.NODE_ENV !== 'test',
+    secure: false,
   })
 );
 
